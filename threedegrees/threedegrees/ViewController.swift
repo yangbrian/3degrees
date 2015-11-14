27589eae3e63f21f2a4dcc6e64f8fcce80cf33cb
Brian Yang <brian@brianyang.me>
2015-11-14 17:36:51 -0500

    Implement basic iOS app functionality

--- threedegrees/threedegrees/ViewController.swift
+++ threedegrees/threedegrees/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    // MARK: Properties
+    @IBOutlet weak var selectButton: UIButton!
+    @IBOutlet weak var selectedImage: UIImageView!
+    @IBOutlet weak var estimatedPercentage: UITextField!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var navigation: UINavigationItem!
+    
+    // MARK: Actions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        navigation.title = "3Degrees"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,48 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // allow user to find an image on their device
+    @IBAction func selectImageAction(sender: UIButton) {
+        estimatedPercentage.resignFirstResponder()
+        
+        // UIImagePickerController is a view controller that lets a user pick media from their photo library.
+        let imagePickerController = UIImagePickerController()
+        
+        // only allow photos to be taken
+        imagePickerController.sourceType = .PhotoLibrary
+        
+        // notify view controller when image picked
+        imagePickerController.delegate = self
+        
+        presentViewController(imagePickerController, animated: true, completion: nil)
+    }
+    
+    // MARK: UIImagePickerControllerDelegate
+    // image selection canceled
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        
+        // Dismiss the picker if the user canceled.
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // User selected an image so display it
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        let selectedImageGallery = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        // Set photoImageView to display the selected image.
+        selectedImage.image = selectedImageGallery
+        
+        // change select image button to say select another
+        selectButton.setTitle("Select another image", forState: .Normal)
+        
+        // only allow submission when an image is selected
+        submitButton.enabled = true
+        
+        // Dismiss the picker.
+        dismissViewControllerAnimated(true, completion: nil)
+        
+    }
 
 }
 
