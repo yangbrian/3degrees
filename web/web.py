015168978caf2c90e54dc9567b138f68d5dff4b4
Brian Yang <brian@brianyang.me>
2015-11-14 17:36:10 -0500

    Add Flask server

---
+++ web/web.py
@@ -0,0 +1,29 @@
+"""
+3Degrees Web Server
+
+Brian Chen
+Nicholas Walsh
+Brian Yang
+
+"""
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+# Just testing web views for now
+@app.route('/image', methods=['GET', 'POST'])
+def image():
+    if request.method == 'POST':
+        return 'Good post request!'
+    else:
+        return 'Good get request!'
+
+if __name__ == '__main__':
+    app.run()
